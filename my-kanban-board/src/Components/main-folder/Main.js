051d90c9035455a7880d22e5b5dd74b65314d95c
Alexander <112536439+avkravchenko@users.noreply.github.com>
2023-05-07 15:17:57 +0400

    1234

--- my-kanban-board/src/Components/main-folder/Main.js
+++ my-kanban-board/src/Components/main-folder/Main.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import './main.scss'
 import Column1 from "./columns-folder/Column1";
-import uuid from "react-uuid";
 import Column2 from "./columns-folder/Column2";
 import Column3 from "./columns-folder/Column3";
+import Column4 from "./columns-folder/Column4";
 
 /* const columns = [
     {title: "Backlog", id: "1"},
@@ -27,13 +27,14 @@ const Main = () => {
                     <Column1 giveSelectedArray={giveSelectedArray} id={"1"}  title={"Backlog"} />
                     <Column2 giveSelectedArray={giveSelectedArray} getSelectedArray={getSelectedArray}  id={"2"}  title={"Ready"} />
                     <Column3 giveSelectedArray={giveSelectedArray} getSelectedArray={getSelectedArray} id={"3"}  title={"In progress"} />
-                    {/* <Column3 id={"3"} key={uuid()} title={"In Progress"} />
-                    <Column4 id={"4"} key={uuid()} title={"Finished"} /> */}
+                    <Column4 giveSelectedArray={giveSelectedArray} getSelectedArray={getSelectedArray} id={"4"}  title={"Finished"} />
                 </div>
             </main>
         </>
     )
 }
+
+
 
 export default Main
 
---
+++ my-kanban-board/src/Components/main-folder/columns-folder/Column4.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import TaskSelect from "./TaskSelect";
+import uuid from "react-uuid";
+import Card from "./Card";
+
+const Column4 = ({ title, getSelectedArray, giveSelectedArray }) => {
+
+    const [isClicked, setIsClicked] = useState(false)
+    const [arrLs, setArrLs] = useState([])
+
+
+    useEffect(() => {
+        const cardsJSON = localStorage.getItem("myObject");
+        if (cardsJSON) {
+            setArrLs(JSON.parse(cardsJSON));
+        }
+    }, [giveSelectedArray]);
+
+    const handleClick = () => {
+        setIsClicked(!isClicked)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const getSelectedValue = (value) => {
+      //делаем новый массив с выбранным опшеном, присваевам значения 
+      const newSelectedValue = [{ id: uuid(), card: value, status: "finished" }];
+      getSelectedArray(newSelectedValue)
+    
+      //получаем массив из лс с карточками из первой и второй колонки 
+      const cardsArray = JSON.parse(localStorage.getItem("myObject"));
+    
+      //создаем новый массив с изменением статуса, чтоб передать его в лс
+      //проходим по каждому значению со статусом бэклог и реди, для каждого бэклога и реди вызываем поиск по новому массиву newSelectedValue
+      //если карточка из лс совпадает со значением нового массива, меняем статус и добавляем в обновленный массив updatedCardsArray
+      const updatedCardsArray = cardsArray.map((card) =>
+        newSelectedValue.find((selected) => selected.card === card.card)
+          ? { ...card, status: "finished" }
+          : { ...card }
+      );
+      localStorage.setItem("myObject", JSON.stringify(updatedCardsArray));
+    };
+      
+
+    return (
+        <form onSubmit={handleSubmit} className="main__content__column">{title}
+        {arrLs.map(value => value.status === "finished" ? <Card key={value.id} card={value.card} /> : null)}
+
+        { isClicked ? 
+            <TaskSelect title={'In progress'} getSelectedValue={getSelectedValue} /> : 
+            null 
+        }
+
+
+        <div><button onClick={handleClick} className="main__content__add-btn">+ Add card</button></div>
+    </form>
+    )
+}
+
+export default Column4
+
